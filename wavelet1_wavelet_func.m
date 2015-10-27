7471f48c13ef8b1395e09c3ab238d2f4c05e0d67
camoomac <camoomac@163.com>
2015-10-27 21:33:07 +0800

    1D_wavelet_part

--- wavelet1_wavelet_func.m
+++ wavelet1_wavelet_func.m
@@ -6,11 +6,17 @@ lamda = [1,0.75,0.6875,0.6719,0.6680,0.6670,0.6668];
 
 h1_j_1 = upsample(h1,2^(scale-1));
 if scale ==1
-    scale_func = signal;
+    scale_func = double(signal);
 else
     scale_func = wavelet1_scale_func( signal,scale-1);
 end
-wave_func = filter(h1_j_1,1,scale_func);
-wave_func = wave_func/lamda(scale+1);
+
+additional_sym = length(signal);
+sym_signal = fliplr(scale_func);
+scale_func_sym = [sym_signal(end-additional_sym+1:end),scale_func,sym_signal(1:additional_sym)];
+
+temp = conv(h1_j_1,scale_func_sym);
+wave_func = temp(additional_sym+1:additional_sym+length(signal));%注意计算小波卷积时，小波系数起点并不是0
+wave_func=wave_func/lamda(scale+1);
 end
 
